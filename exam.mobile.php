c869f996e57c2f8c3049e65b9586744bdbeb2299
BJRambo <qw5414@naver.com>
2017-06-04 20:21:36 +0900

    init module commit

---
+++ exam.mobile.php
@@ -0,0 +1,21 @@
+<?php
+require_once(_XE_PATH_ . 'modules/exam/exam.view.php');
+
+class examMobile extends examView
+{
+	function init()
+	{
+		$template_path = sprintf("%sm.skins/%s/", $this->module_path, $this->module_info->mskin);
+		if(!is_dir($template_path) || !$this->module_info->mskin)
+		{
+			$this->module_info->mskin = 'default';
+			$template_path = sprintf("%sm.skins/%s/", $this->module_path, $this->module_info->mskin);
+		}
+		$this->setTemplatePath($template_path);
+
+
+
+
+
+	}
+}
